03154de8132188ee06dfa2b2a222dc4db2964437
LucaCiucci <luca.ciucci99@gmail.com>
2024-10-24 12:02:32 +0200

    Remove old `receive_frame` function

--- crates/birb-vision-mvs/src/birb_vision_impl.rs
+++ crates/birb-vision-mvs/src/birb_vision_impl.rs
@@ -109,23 +109,6 @@ impl CameraDevice for MVDevice {
         Ok(())
     }
 
-    //async fn receive_frame(&self) -> DeviceResult<std::borrow::Cow<'_, Frame>> {
-    //    // TODO HANDLE DIFFERENT PIXEL FORMATS
-//
-    //    let w = self.get_int_value("Width").map_err(|e| e.into())?.current();
-    //    let h = self.get_int_value("Height").map_err(|e| e.into())?.current();
-    //    //let pitch = self.get_int_value("LinePitch").map_err(|e| e.into())?.current();
-    //    //assert_eq!(pitch, w, "LinePitch != Width");
-//
-    //    let mut buf = ImageBuffer::<Luma<u8>, Vec<u8>>::new(w as u32, h as u32).into_raw();
-//
-    //    self.get_one_frame_timeout(&mut buf, Duration::from_secs(1)).map_err(|e| e.into())?;
-//
-    //    let img = DynamicImage::ImageLuma8(ImageBuffer::from_raw(w as u32, h as u32, buf).unwrap());
-//
-    //    Ok(Cow::Owned(Frame::Image(img)))
-    //}
-
     fn grab(&self) -> DeviceResult<()> {
         // TODO this function is deprecated, what should we use instead? Maybe MV_CC_SetCommandValue
         mvs_try!(self.cx => MV_CC_TriggerSoftwareExecute(self.handle)).map_err(|e| e.into())
