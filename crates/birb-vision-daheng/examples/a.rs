e1a7bba0d159295e2f3afe1247e9dad7e27a68e6
LucaCiucci <luca.ciucci99@gmail.com>
2024-11-02 12:02:55 +0100

    WIP on daheng camera support

--- crates/birb-vision-daheng/examples/a.rs
+++ crates/birb-vision-daheng/examples/a.rs
@@ -1,7 +1,22 @@
-use birb_vision_daheng::Ctx;
+use std::time::Duration;
+
+use birb_vision_daheng::{Ctx, Device};
+use birb_vision_core::CameraDevice;
 
 fn main() {
     let ctx = Ctx::new().unwrap();
     let n = ctx.get_all_device_base_info().unwrap().len();
     println!("Found {n} devices");
+
+    for info in ctx.get_all_device_base_info().unwrap() {
+        println!("Opening device: {:?}", info.model_name());
+        let dev = Device::open(info).unwrap();
+        dev.set_stream_callback(Box::new(|e| {
+            println!("Event: {e:?}");
+        })).unwrap();
+        println!("Grabbing...");
+        dev.start_grabbing().unwrap();
+        std::thread::sleep(Duration::from_secs(5));
+        dev.stop_grabbing().unwrap();
+    }
 }
