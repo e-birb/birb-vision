01d34adb071ff6050dcb8c1a463a03bfff1af65d
Luca Ciucci <luca.ciucci99@gmail.com>
2025-02-18 19:12:23 +0100

    Minor fix

--- crates/birb-vision/src/lib.rs
+++ crates/birb-vision/src/lib.rs
@@ -37,7 +37,8 @@ pub fn all_backends() -> BackendRegistry {
     #[cfg(feature = "media-foundation")]
     set.register(
         BackendPackage::from_builder_fn(|| {
-            let ctx = birb_vision_media_foundation::MediaFoundationContext::new().map_err(|e| anyhow!("Failed to create MediaFoundationContext: {e}"))?;
+            let ctx = birb_vision_media_foundation::MediaFoundationContext::new()
+                .map_err(|e| anyhow::anyhow!("Failed to create MediaFoundationContext: {e}"))?;
             Ok(ctx)
         })
         .with_display_name("Media Foundation")
